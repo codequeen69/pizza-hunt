f8d1327d0fe68abbea87ac542d187abda3a69382
Venus DM <crhowe90@gmail.com>
2022-09-19 11:51:51 -0500

    comments working

---
+++ controllers/comment-controller.js
@@ -0,0 +1,55 @@
+const { Comment, Pizza } = require('../models');
+
+
+const commentController = {
+    //add a comment to a pizza
+    addComment({ params, body }, res){
+        console.log(body);
+        Comment.create(body)
+
+        .then(({_id}) => {
+           return Pizza.findOneAndUpdate(
+               {_id: params.pizzaId},
+               //we use the MongoDB $push operator to add the comment's _id to the specific pizza we want to update
+               //$push works just like it does in JavaScript by adding data to an array
+               { $push: { comments: _id}},
+               { new: true }
+           );
+        })
+        .then(dbPizzaData => {
+            if(!dbPizzaData){
+                res.status(404).json({ message: 'No pizza found with this id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        }) 
+        .catch(err => res.json(err));
+
+    },
+
+    //remove a comment
+    removeComment({params}, res){
+        Comment.findOneAndDelete({_id: params.commentId})
+        .then(deletedComment =>{
+            if(!deletedComment){
+                return res.status(404).json({ message:'No comment with this id!'});
+            }
+            return Pizza.findOneAndUpdate(
+                { _id: params.pizzaId },
+                //MongoDB $pull removes item from array
+                { $pull: { comments: params.commentId}},
+                {new: true }
+            );
+        })
+        .then(dbPizzaData => {
+            if(!dbPizzaData){
+                res.status(404).json({ message: 'No pizza found with this id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    }
+};
+
+module.exports = commentController;
